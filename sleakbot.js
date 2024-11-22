77ba362ca7c94dfa2eb8b216e4e2c5d4ebfd3421
sleakchat <support@sleak.chat>
2024-11-22 22:09:58 +0100

    Refactor cookie creation in sleakScript to directly assign visitorId from createNewCookie

--- sleakbot.js
+++ sleakbot.js
@@ -36,8 +36,8 @@ async function sleakScript() {
       const urlParams = new URLSearchParams(window.location.search);
       if (urlParams.has('resetChat')) {
         Cookies.remove(`sleakVisitorId_${chatbotId}`);
-        createNewCookie(`sleakVisitorId_${chatbotId}`, crypto.randomUUID());
-        visitorId = Cookies.get(`sleakVisitorId_${chatbotId}`);
+        visitorId = createNewCookie(`sleakVisitorId_${chatbotId}`, crypto.randomUUID());
+        // visitorId = Cookies.get(`sleakVisitorId_${chatbotId}`);
         urlParams.delete('resetChat');
         const updatedParams = urlParams.toString();
         const newUrl = updatedParams ? `${window.location.origin}${window.location.pathname}?${updatedParams}` : `${window.location.origin}${window.location.pathname}`;
@@ -46,8 +46,8 @@ async function sleakScript() {
 
       visitorId = Cookies.get(`sleakVisitorId_${chatbotId}`);
     } else {
-      createNewCookie(`sleakVisitorId_${chatbotId}`, crypto.randomUUID());
-      visitorId = Cookies.get(`sleakVisitorId_${chatbotId}`);
+      visitorId = createNewCookie(`sleakVisitorId_${chatbotId}`, crypto.randomUUID());
+      // visitorId = Cookies.get(`sleakVisitorId_${chatbotId}`);
       // console.log("new cookie = ", visitorId);
     }
   } else {
