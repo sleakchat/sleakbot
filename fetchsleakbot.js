f60789f9e4a224a03bcf5b7b994d3b48b4d7086a
sleakchat <support@sleak.chat>
2025-01-09 15:30:21 +0100

    add shadow dom

--- fetchsleakbot.js
+++ fetchsleakbot.js
@@ -27,18 +27,18 @@ async function injectSleakScript() {
   const sleakJs = `${baseUrl}/${fileName}.js`;
   const sleakCss = `${baseUrl}/${fileName}.css`;
 
-  async function appendStylesheet(url) {
-    var link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.type = 'text/css';
-    link.href = url;
-    document.head.appendChild(link);
-  }
-  appendStylesheet(sleakCss);
-
   // append div to body
   function appendSleakHtmlToBody(sleak_html) {
     const sleakHtml = document.createElement('div');
+    const shadowRoot = sleakContainer.attachShadow({ mode: 'open' });
+    async function appendStylesheet(url) {
+      var link = shadowRoot.createElement('link');
+      link.rel = 'stylesheet';
+      link.type = 'text/css';
+      link.href = url;
+      document.head.appendChild(link);
+    }
+    appendStylesheet(sleakCss);
     sleakHtml.innerHTML = sleak_html;
     if (placement === 'fullwidth') {
       sleakHtml.style.width = '100%';
