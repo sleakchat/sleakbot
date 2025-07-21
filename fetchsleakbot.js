ef139b3cd16a3bca0b9fb1ffa252aaab470d6d3c
sleakchat <support@sleak.chat>
2025-07-21 16:53:13 +0200

    modular populatePopup function

--- fetchsleakbot.js
+++ fetchsleakbot.js
@@ -394,9 +394,14 @@
           }
         }
 
-        slkPopupAvatar.src = chatbotConfig.avatar_url;
-        slkPopupAgentName.textContent = chatbotConfig.name;
-        slkPopupBodyMessage.textContent = chatbotConfig.first_message;
+        window.populatePopup = function (avatar, name, message) {
+          slkPopupAvatar.src = avatar;
+          slkPopupAgentName.textContent = name;
+          slkPopupBodyMessage.textContent = message;
+        };
+
+        // Set initial popup content
+        window.populatePopup(chatbotConfig.avatar_url, chatbotConfig.name, chatbotConfig.first_message);
 
         function openSleakWidget() {
           sleakEmbeddedWidget.style.display = 'flex';
@@ -558,10 +563,7 @@
 
           document.documentElement.style.setProperty('--sleak-loading-dot-color', chatbotConfig.primary_color);
 
-          // populate default popup
-          slkPopupAvatar.src = payload.avatar;
-          slkPopupAgentName.textContent = payload.name;
-          slkPopupBodyMessage.textContent = pagePopup.message;
+          window.populatePopup(payload.avatar, payload.name, pagePopup.message);
 
           liveChatPopup.querySelector('#sleak-operatorchanged-avatar').src = payload.avatar;
           liveChatPopup.querySelector('#sleak-operatorchanged-name').innerText = payload.name;
@@ -737,9 +739,7 @@
           } else if (event.data.type === 'resetChat') {
             window.resetVisitorId();
           } else if (event.data.type === 'showMessagePopup') {
-            slkPopupAvatar.src = event.data.payload.avatar;
-            slkPopupAgentName.textContent = event.data.payload.name;
-            slkPopupBodyMessage.textContent = event.data.payload.message;
+            window.populatePopup(event.data.payload.avatar, event.data.payload.name, event.data.payload.message);
             showPopup();
           } else {
             if (event.data.type !== 'showOutputLogsAdmin') console.log('no declared event');
